8ce4dec447f85b291b84731527971bc767ea8683
luetkemj <luetkemj@gmail.com>
2019-11-17 15:20:06 -0600

    Updates

---
+++ precache-manifest.e5bd276b2664e5153c284d482bf0a678.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d69292fb6124b9ec289b28dfbec801e",
+    "url": "/snerv/index.html"
+  },
+  {
+    "revision": "b859e40788e855d37bc0",
+    "url": "/snerv/static/css/main.5932151f.chunk.css"
+  },
+  {
+    "revision": "c4f88127802894323fcf",
+    "url": "/snerv/static/js/2.f5244bb1.chunk.js"
+  },
+  {
+    "revision": "b859e40788e855d37bc0",
+    "url": "/snerv/static/js/main.fc68ae12.chunk.js"
+  },
+  {
+    "revision": "fabd71acb2b80c3029eb",
+    "url": "/snerv/static/js/runtime-main.1522390c.js"
+  }
+]);
